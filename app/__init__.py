c4723def261036dea55cd767680f803db7470597
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-02-19 11:37:50 +0200

    creating migration files

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@
 from flask import  Flask
 from app.config import config_options as AppConfig
 from app.models import  db
+from flask_migrate import Migrate
+
 
 def create_app(config_name='dev'):
     # create app
@@ -12,7 +14,24 @@ def create_app(config_name='dev'):
     ## read database configuration from config class
     app.config['SQLALCHEMY_DATABASE_URI'] = current_config.SQLALCHEMY_DATABASE_URI
 
+
+
+    # define db to the app
     db.init_app(app)
+    ## init migration
+    migrate = Migrate(app, db, render_as_batch=True)
+
+    ## in project ---> 1- init migration directory
+    """
+        cd app 
+        export FLASK_APP=flasky
+        flask db init  ## initialize migration directory
+        # to create migration file
+        flask db migrate -m 'migrate message '
+        # to apply changes to database
+        flask db upgrade         
+    """
+
 
 
 
--- app/models.py
+++ app/models.py
@@ -2,3 +2,18 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
+
+class Student(db.Model):
+    __tablename__ = 'students'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String, nullable=True)
+    grade= db.Column(db.Integer, nullable=True)
+
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def get_all_objects(cls):
+        return cls.query.all()
